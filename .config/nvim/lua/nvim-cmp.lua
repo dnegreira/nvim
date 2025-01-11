8694d2aeceae63078a973c441b84a7cc0e2d5969
David Negreira <David@otherreality.net>
2025-01-11 23:42:34 +0000

    add unusedparams

---
+++ .config/nvim/lua/nvim-cmp.lua
@@ -0,0 +1,118 @@
+-- import nvim-cmp plugin safely
+local cmp_status, cmp = pcall(require, "cmp")
+if not cmp_status then
+	return
+end
+
+-- import luasnip plugin safely
+local luasnip_status, luasnip = pcall(require, "luasnip")
+if not luasnip_status then
+	return
+end
+
+-- import lspkind plugin safely
+
+local lspkind = require('lspkind')
+-- load VSCode-like snippets from plugins (e.g., friendly-snippets)
+
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+vim.opt.completeopt = "menu,menuone,noselect"
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require('lspconfig')
+local servers = { 'gopls' }
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup {
+		-- on_attach = my_custom_on_attach,
+		capabilities = capabilities,
+		settings = {
+			gopls = {
+				usePlaceholders = true,
+			},
+			analyses = {
+				unusedparams = true,
+			},
+		},
+	}
+end
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+cmp.setup({
+	view = {
+		entries = "custom"
+	},
+	formatting = {
+		format = lspkind.cmp_format({
+			-- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+			mode = 'symbol_text', -- show only symbol annotations
+			maxwidth = {
+				-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+				-- can also be a function to dynamically calculate max width such as
+				-- menu = function() return math.floor(0.45 * vim.o.columns) end,
+				menu = 50, -- leading text (labelDetails)
+				abbr = 50, -- actual suggestion item
+			},
+			ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			before = function(entry, vim_item)
+				-- ...
+				return vim_item
+			end
+		})
+	},
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+		['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+		-- C-b (back) C-f (forward) for snippet placeholder navigation.
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<CR>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+		['<Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+		['<S-Tab>'] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { 'i', 's' }),
+	}),
+	--	mapping = cmp.mapping.preset.insert({
+	--		["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+	--		["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+	--		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+	--		["<C-f>"] = cmp.mapping.scroll_docs(4),
+	--		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+	--		["<C-e>"] = cmp.mapping.abort(), -- close completion window
+	--		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	--	}),
+	-- sources for autocompletion
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" }, -- LSP
+		{ name = "luasnip" }, -- snippets
+		{ name = "buffer" }, -- text within the current buffer
+		{ name = "path" }, -- file system paths
+	}),
+})
